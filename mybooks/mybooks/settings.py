74da7af40302f7af4249379d392ff38f06496ed9
sunhuang163 <sunhuang2005@163.com>
2016-08-22 22:15:18 +0800

    models

--- mybooks/mybooks/settings.py
+++ mybooks/mybooks/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+EXTERNAL_LIBS_PATH = os.path.join(
+    BASE_DIR, "externals", "libs"
+)
+EXTERNAL_APPS_PATH = os.path.join(
+    BASE_DIR, "externals", "apps"
+)
+sys.path = ["", EXTERNAL_LIBS_PATH, EXTERNAL_APPS_PATH] + sys.path
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
